14bb1d15d250c42605836056189d0cafdd07998f
Jeffrey Hu <zhiwehu@gmail.com>
2014-09-04 01:01:46 +0800

    新的disqus lib

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
+window.disqus_shortname = 'lettoo';
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('app', [
     'ngRoute',
     'hc.marked',
+    'ngDisqus',
     'app.filters',
     'app.services',
     'app.directives',
